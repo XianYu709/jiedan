922c8a00cd6584da162de8d0f585ea76202729bb
XianYu <gaobh8@gmail.com>
2025-04-18 15:25:24 +0800

    feat(td-map): 增加设施查询功能
    - 添加设施查询表格，显示名称和容量信息- 实现矩形和圆形区域的设施查询
    - 优化查询结果的展示方式，增加标记和信息窗口
    - 调整查询类型和清除功能的逻辑

--- web/src/views/td-map/components/FacilityInquiry.vue
+++ web/src/views/td-map/components/FacilityInquiry.vue
@@ -12,19 +12,39 @@
       <Radio.Button value="防空洞">防空洞</Radio.Button>
       <Radio.Button value="避难所">避难所</Radio.Button>
       <Radio.Button value="消防站">消防站</Radio.Button>
-      <Radio.Button value="clear" @click="clear(true)"><span
-        class="iconfont icon-clear" style="font-size: 15px"/>
+      <Radio.Button value="clear" @click="clear(true)">
+        <CloseCircleOutlined/>
       </Radio.Button>
     </Radio.Group>
+    <Table v-if="dataSouce.length>0"
+           :columns="[{
+      title: '名称',
+      dataIndex: 'name',
+      key: 'name',
+    },   {
+      title: '容量',
+      dataIndex: 'capacity',
+      key: 'capacity',
+      width: 100,
+    }]"
+           :data-source="dataSouce"
+           :pagination="{
+       pageSize:5
+     }"
+    >
+
+    </Table>
+    <Empty v-else class="mt-6"/>
   </Dialog>
 </template>
 <script lang="ts" setup>
-import {inject, ref, watch} from 'vue';
+import {inject, ref, shallowRef, watch} from 'vue';
 import Dialog from './Dialog.vue';
-import {Radio} from "ant-design-vue";
+import {Radio, Table,Empty} from "ant-design-vue";
+import {CloseCircleOutlined} from "@ant-design/icons-vue";
 
 const drawType = ref('rectangle');
-const inquiryType = ref('医院');
+const inquiryType = ref('clear');
 
 const props = defineProps({
   open: {type: Boolean, default: false},
@@ -33,7 +53,9 @@ const emits = defineEmits(['update:open']);
 const open = ref(false);
 const instance: any = inject('instance');
 
-let AMap, map, mouseTool, overlays = [];
+let AMap, map, mouseTool, placeSearch, overlays = []
+const dataSouce = ref([])
+const marks = shallowRef([])
 
 const handleCancel = () => {
   open.value = false;
@@ -43,12 +65,64 @@ const handleCancel = () => {
 const clear = (flag = false) => {
   map.remove(overlays)
   overlays = [];
-  if (flag) {
-    setTimeout(() => {
-      inquiryType.value = '医院';
-      draw()
+  if (marks.value.length > 0) {
+    marks.value.forEach(item => {
+      item.setMap(null)
     })
+    marks.value = []
   }
+  dataSouce.value = []
+  mouseTool && mouseTool.close();
+}
+
+const drawEnd = () => {
+  const bounds = overlays[0].getBounds()
+  AMap.plugin(['AMap.PlaceSearch'], function () {
+    placeSearch = new AMap.PlaceSearch({
+      city: '370100',
+      size: 99
+    });
+    placeSearch.search(inquiryType.value, function (status, result) {
+      if (status === 'complete') {
+        const filtered = result.poiList.pois.filter((poi: any) =>
+          bounds.contains([poi.location.lng, poi.location.lat])
+        );
+        filtered.forEach(item => {
+          const marker = new AMap.Marker({
+            position: item.location,
+            title: item.name,
+          });
+          marker.on('click', () => {
+          });
+          marker.setMap(map);
+          marks.value.push(marker)
+        });
+        next(filtered)
+      }
+    })
+  });
+}
+
+const next = (marks) => {
+  dataSouce.value = marks.map(item => {
+    return {
+      name: item.name,
+      capacity: '未知',
+    }
+  })
+}
+
+const initDraw = () => {
+  if (!AMap) return;
+  mouseTool = mouseTool ? mouseTool : new AMap.MouseTool(map);
+  mouseTool.on('draw', function (e: any) {
+    if (overlays.length > 0) {
+      clear()
+    }
+    overlays.push(e.obj);
+    drawEnd()
+  })
+  draw()
 }
 
 watch(
@@ -58,16 +132,6 @@ watch(
     if (open.value) {
       AMap = instance.value.AMap;
       map = instance.value.map;
-      mouseTool = new AMap.MouseTool(map);
-      mouseTool.on('draw', function (e:any) {
-        console.log(overlays.length)
-        console.log(e)
-        if (overlays.length > 0) {
-          clear()
-        }
-        overlays.push(e.obj);
-      })
-      draw()
     } else {
       mouseTool.close();
     }
@@ -77,6 +141,7 @@ watch(
 
 const changeHandler = () => {
   clear()
+  initDraw()
   draw()
 }
 
@@ -84,15 +149,15 @@ const draw = () => {
   switch (drawType.value) {
     case 'rectangle': {
       mouseTool.rectangle({
-        fillColor: '#00b0ff',
-        strokeColor: '#80d8ff',
+        fillColor: '#97d8f8',
+        strokeColor: '#0dabf1',
       });
       break;
     }
     case 'circle': {
       mouseTool.circle({
-        fillColor: '#00b0ff',
-        strokeColor: '#80d8ff',
+        fillColor: '#97d8f8',
+        strokeColor: '#0dabf1',
       });
       break;
     }
