76a817be68199a2f366abac8ebdf0ff9e7996fa7
XianYu <gaobh8@gmail.com>
2025-04-18 10:05:16 +0800

    feat(td-map): 优化设施查询功能
    - 新增矩形和圆形两种查询方式
    - 添加清除查询结果的功能
    - 优化查询界面布局和样式
    - 改进查询逻辑，支持动态切换查询类型

--- web/src/views/td-map/components/FacilityInquiry.vue
+++ web/src/views/td-map/components/FacilityInquiry.vue
@@ -1,10 +1,20 @@
 <template>
-  <Dialog v-model:open="open" :width="340" title="设施查询" @cancel="handleCancel">
-    <Radio.Group class="mt-4 ">
-      <Radio.Button value="large">医院</Radio.Button>
-      <Radio.Button value="default">防空洞</Radio.Button>
-      <Radio.Button value="small">避难所</Radio.Button>
-      <Radio.Button value="small">消防站</Radio.Button>
+  <Dialog v-model:open="open" :width="380" title="设施查询" @cancel="handleCancel">
+    <template #extra>
+      <Radio.Group v-model:value="drawType" button-style="solid" size="small"
+                   @change="changeHandler">
+        <Radio.Button value="rectangle">矩形</Radio.Button>
+        <Radio.Button value="circle">圆形</Radio.Button>
+      </Radio.Group>
+    </template>
+    <Radio.Group v-model:value="inquiryType" class="mt-4" @change="changeHandler">
+      <Radio.Button value="医院">医院</Radio.Button>
+      <Radio.Button value="防空洞">防空洞</Radio.Button>
+      <Radio.Button value="避难所">避难所</Radio.Button>
+      <Radio.Button value="消防站">消防站</Radio.Button>
+      <Radio.Button value="clear" @click="clear(true)"><span
+        class="iconfont icon-clear" style="font-size: 15px"/>
+      </Radio.Button>
     </Radio.Group>
   </Dialog>
 </template>
@@ -13,6 +23,8 @@ import {inject, ref, watch} from 'vue';
 import Dialog from './Dialog.vue';
 import {Radio} from "ant-design-vue";
 
+const drawType = ref('rectangle');
+const inquiryType = ref('医院');
 
 const props = defineProps({
   open: {type: Boolean, default: false},
@@ -20,70 +32,24 @@ const props = defineProps({
 const emits = defineEmits(['update:open']);
 const open = ref(false);
 const instance: any = inject('instance');
-let AMap, map, mouseTool;
 
-const draw = (type) => {
-  switch (type) {
-    case 'rule': {
-      mouseTool.rule({
-        startMarkerOptions: {
-          //可缺省
-          icon: new AMap.Icon({
-            size: new AMap.Size(19, 31), //图标大小
-            imageSize: new AMap.Size(19, 31),
-            image: '//webapi.amap.com/theme/v1.3/markers/b/start.png',
-          }),
-          offset: new AMap.Pixel(-9, -31),
-        },
-        endMarkerOptions: {
-          //可缺省
-          icon: new AMap.Icon({
-            size: new AMap.Size(19, 31), //图标大小
-            imageSize: new AMap.Size(19, 31),
-            image: '//webapi.amap.com/theme/v1.3/markers/b/end.png',
-          }),
-          offset: new AMap.Pixel(-9, -31),
-        },
-        midMarkerOptions: {
-          //可缺省
-          icon: new AMap.Icon({
-            size: new AMap.Size(19, 31), //图标大小
-            imageSize: new AMap.Size(19, 31),
-            image: '//webapi.amap.com/theme/v1.3/markers/b/mid.png',
-          }),
-          offset: new AMap.Pixel(-9, -31),
-        },
-        lineOptions: {
-          //可缺省
-          strokeStyle: 'solid',
-          strokeColor: '#FF33FF',
-          strokeOpacity: 1,
-          strokeWeight: 2,
-        },
-        //同 RangingTool 的 自定义 设置，缺省为默认样式
-      });
-      break;
-    }
-    case 'measureArea': {
-      mouseTool.measureArea({
-        strokeColor: '#80d8ff',
-        fillColor: '#80d8ff',
-        fillOpacity: 0.3,
-        //同 Polygon 的 Option 设置
-      });
-      break;
-    }
-  }
-};
-
-const clear = () => {
-  mouseTool.close(true); //关闭，并清除覆盖物
-};
+let AMap, map, mouseTool, overlays = [];
 
 const handleCancel = () => {
   open.value = false;
   emits('update:open', false);
 };
+
+const clear = (flag = false) => {
+  map.remove(overlays)
+  overlays = [];
+  if (flag) {
+    setTimeout(() => {
+      inquiryType.value = '医院';
+      draw()
+    })
+  }
+}
 
 watch(
   () => props,
@@ -93,9 +59,45 @@ watch(
       AMap = instance.value.AMap;
       map = instance.value.map;
       mouseTool = new AMap.MouseTool(map);
+      mouseTool.on('draw', function (e:any) {
+        console.log(overlays.length)
+        console.log(e)
+        if (overlays.length > 0) {
+          clear()
+        }
+        overlays.push(e.obj);
+      })
+      draw()
+    } else {
+      mouseTool.close();
     }
   },
   {deep: true},
 );
+
+const changeHandler = () => {
+  clear()
+  draw()
+}
+
+const draw = () => {
+  switch (drawType.value) {
+    case 'rectangle': {
+      mouseTool.rectangle({
+        fillColor: '#00b0ff',
+        strokeColor: '#80d8ff',
+      });
+      break;
+    }
+    case 'circle': {
+      mouseTool.circle({
+        fillColor: '#00b0ff',
+        strokeColor: '#80d8ff',
+      });
+      break;
+    }
+  }
+}
+
 </script>
 <style lang="less" scoped></style>
