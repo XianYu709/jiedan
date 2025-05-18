596904c5321312c6c99538d18e2dad7725ca708a
邢继森 <1933694610@qq.com>
2025-05-18 17:27:39 +0800

    可视域表单

--- src/views/map/components/viewshed/index.vue
+++ src/views/map/components/viewshed/index.vue
@@ -14,6 +14,19 @@ export default {
       default: false
     }
   },
+  data() {
+    return {
+      formInline: {
+        way: 0,
+        flip: 0,
+        range: 0,
+        level: 0,
+        vertical: 0,
+        vis: '#FF0000',
+        invisible: '#00FF00'
+      }
+    }
+  },
   computed: {
     computedVisible: {
       get() {
@@ -48,6 +61,29 @@ export default {
     show-close
   >
     <div style="text-align: center;">
+      <el-form label-position="right" label-width="130px" :model="formInline">
+        <el-form-item label="方向(度): ">
+          <el-slider v-model="formInline.way"/>
+        </el-form-item>
+        <el-form-item label="翻转(度): ">
+          <el-slider v-model="formInline.flip"/>
+        </el-form-item>
+        <el-form-item label="距离(米): ">
+          <el-slider v-model="formInline.range"/>
+        </el-form-item>
+        <el-form-item label="水平视场角(度): ">
+          <el-slider v-model="formInline.level"/>
+        </el-form-item>
+        <el-form-item label="垂直视场角(度): ">
+          <el-slider v-model="formInline.vertical"/>
+        </el-form-item>
+        <el-form-item label="可见区域颜色: ">
+          <el-color-picker v-model="formInline.vis" style="margin-left: -260px;"/>
+        </el-form-item>
+        <el-form-item label="不可见区域颜色: ">
+          <el-color-picker v-model="formInline.invisible" style="margin-left: -260px;"/>
+        </el-form-item>
+      </el-form>
       <el-button type="primary" size="small" @click="handleDraw">绘制</el-button>
       <el-button size="small" @click="handleClear">清空</el-button>
     </div>
